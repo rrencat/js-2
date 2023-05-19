107ec209f116002d9df4f6fa534075b930a92642
rrencat <104782737+rrencat@users.noreply.github.com>
2023-05-19 18:55:32 +0200

    constants

--- src/js/api/fetchToken.js
+++ src/js/api/fetchToken.js
@@ -1,23 +1,17 @@
-import displayMessage from "../ui/common/displayMessage";
+import { load } from "../storage/index.js";
 
-/**
- * Fetches data from a specified endpoint using the provided options.
- *
- * @param {string} API_SOCIAL_URL - The base URL of the API.
- * @param {string} endpoint - The endpoint to fetch the data from.
- * @param {object} options - (Optional) Additional options for the fetch request.
- * @returns {void}
- */
-export async function fetchData(API_SOCIAL_URL, endpoint, options = {}) {
-  try {
-    const response = await fetch(`${API_SOCIAL_URL}${endpoint}`, options);
+export function headers() {
+    const token = load("token");
 
-    if (response.status === 201) {
-      displayMessage("success", "User registered");
-    } else {
-      displayMessage("warning", "User already exists");
+    return {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
     }
-  } catch (error) {
-    displayMessage("warning", error);
-  }
+}
+
+export async function fetchToken(url, options = {}) {
+    return fetch(url, {
+        ...options,
+        headers: headers()
+    })
 }
