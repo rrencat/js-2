0164c548bcf0436b2a085765ab3d0e77cdadde4a
rrencat <104782737+rrencat@users.noreply.github.com>
2023-05-25 21:50:36 +0200

    Create getPostId.js

---
+++ src/js/handlers/posts/getPostId.js
@@ -0,0 +1,8 @@
+export function getPostId() {
+    const queryString = document.location.search;
+    const parameter = new URLSearchParams(queryString);
+    const id = parameter.get("id");
+    return id;
+  }
+  
+  getPostId();
